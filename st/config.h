1afe3716c99ed866d76afcd53ff9749ad89e3893
Add Chan <addison95132@gmail.com>
2016-10-16 13:11:43 -0700

    st update

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Inconsolata:pixelsize=22:antialias=true:autohint=true";
+static char font[] = "Inconsolata:pixelsize=23:antialias=true:autohint=true";
 static int borderpx = 2;
 #define histsize 2000
 
