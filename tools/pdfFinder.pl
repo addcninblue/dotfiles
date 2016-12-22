529e32cabdeddf54077897dfc9093178c5a1050e
Add Chan <addison95132@gmail.com>
2016-12-21 19:31:53 -0800

    make a pdf searcher

--- tools/pdfFinder.pl
+++ tools/pdfFinder.pl
@@ -7,7 +7,7 @@ my $pdflist = `find ~ -name "*.pdf"`;
 my @pdfs= split(/\n/, $pdflist);
 my @pdfNames = $pdflist =~ m#([^\/]*pdf)#g;
 $pdflist = join("\n", @pdfNames);
-my $file = `echo "$pdflist" | rofi -dmenu run -lines 4 -bw 20 -eh 2 -width 600 -padding 40 -p "pdf> "`;
+my $file = `echo "$pdflist" | rofi -dmenu run -lines 4 -bw 20 -eh 2 -width 600 -padding 40 -i -p "pdf> "`;
 chomp($file);
 
 my ($output) = grep(/\Q$file\E/, @pdfs);
